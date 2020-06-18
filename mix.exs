66607a0b27ede5fd53d8f378a7a8fbc587d339c1
Makenna Smutz <makennasmutz@gmail.com>
2020-06-19 01:58:28 +0200

    formatting

--- mix.exs
+++ mix.exs
@@ -2,14 +2,16 @@ defmodule Community.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :community,
-     version: "0.0.1",
-     elixir: "~> 1.4",
-     elixirc_paths: elixirc_paths(Mix.env),
-     compilers: [:phoenix, :gettext] ++ Mix.compilers,
-     start_permanent: Mix.env == :prod,
-     aliases: aliases(),
-     deps: deps()]
+    [
+      app: :community,
+      version: "0.0.1",
+      elixir: "~> 1.4",
+      elixirc_paths: elixirc_paths(Mix.env),
+      compilers: [:phoenix, :gettext] ++ Mix.compilers,
+      start_permanent: Mix.env == :prod,
+      aliases: aliases(),
+      deps: deps()
+    ]
   end
 
   # Configuration for the OTP application.
